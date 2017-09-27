13087c4ccfae7c50f76c1bf017420b6cb876882e
sevendollar <sevendollar@gmail.com>
2017-09-27 06:53:54 +0000

    minor bug tuned

--- modules/vmware-vm/main.tf
+++ modules/vmware-vm/main.tf
@@ -1,8 +1,7 @@
 resource "vsphere_virtual_machine" "etcd" {
 #  count = "${var.env == "prodution" ? "${var.count}" : "0" }"
   count = "${var.count}"
-#  name = "${var.datacenter}-coreos-${var.name}-${count.index+1}"
-  name = "${var.datacenter}-coreos-${md5("${count.index}")}"
+  name = "${var.datacenter}-coreos-${md5("${var.name}${count.index}")}"
   vcpu = "${var.vcpu}"
   memory = "${var.memory}"
   datacenter = "${var.datacenter}"
@@ -21,7 +20,7 @@ resource "vsphere_virtual_machine" "etcd" {
 #    template = "taiping_image_coreos"
     type = "thin"
     size = "16"
-    name = "vmdk-coreos-${md5("${count.index}")}"
+    name = "vmdk-coreos-${md5("${var.name}${count.index}")}"
     datastore = "tp_NetApp_flra_vmware"
   }
 }
